61504a0c3ed4091ba1fefbd83d497198897f1c30
Anil <anilflyto@gmail.com>
2023-01-03 22:31:38 +0530

    merged all state to once

--- src/components/CustomProgress.js
+++ src/components/CustomProgress.js
@@ -3,35 +3,61 @@ import { Button, ProgressBar } from 'react-bootstrap'
 
 const CustomProgress = () => {
 
-    const [progressStatus, setProgressStatus] = useState(0)
-    const [disableAdd, setDisableAdd] = useState(false)
-    const [disableDesc, setDisableDesc] = useState(false)
+    // const [progressStatus, setProgressStatus] = useState(0)
+    // const [disableAdd, setDisableAdd] = useState(false)
+    // const [disableDesc, setDisableDesc] = useState(false)
+
+    const [properties, setProperties] = useState({
+        progressStatus: 0,
+        disableAdd: false,
+        disableDesc: false
+    })
 
 
     const handleAdd = () => {
-        if (progressStatus < 100) {
-            setDisableDesc(false)
-            setProgressStatus(progressStatus + 10)
+        if (properties.progressStatus < 100) {
+            // setDisableDesc(false)
+            // setProgressStatus(progressStatus + 10)
+            setProperties({
+                ...properties,
+                disableDesc: false,
+                progressStatus: properties.progressStatus + 10
+            })
         } else {
-            setDisableDesc(false)
-            setDisableAdd(true)
+            // setDisableDesc(false)
+            // setDisableAdd(true)
+            setProperties({
+                ...properties,
+                disableDesc: false,
+                disableAdd: true
+            })
         }
     }
     const handleDesc = () => {
-        if (progressStatus > 0) {
-            setDisableAdd(false)
-            setProgressStatus(progressStatus - 10)
+        if (properties.progressStatus > 0) {
+            // setDisableAdd(false)
+            // setProgressStatus(progressStatus - 10)
+            setProperties({
+                ...properties,
+                disableAdd: false,
+                progressStatus: properties.progressStatus - 10
+            })
         } else {
-            setDisableAdd(false)
-            setDisableDesc(true)
+            // setDisableAdd(false)
+            // setDisableDesc(true)
+            setProperties({
+                ...properties,
+                disableAdd: false,
+                disableDesc: true
+            })
         }
     }
 
     return (
         <div className='p-4'>
-            <ProgressBar label={`${progressStatus}%`} striped variant="success" now={progressStatus} className="mb-4" />
-            <Button variant="danger" className='m-4' onClick={handleDesc} disabled={disableDesc}>Decrease</Button>
-            <Button variant="success" onClick={handleAdd} disabled={disableAdd}>Increase</Button>
+            <ProgressBar label={`${properties.progressStatus}%`} striped variant="success" now={properties.progressStatus} className="mb-4" />
+            <Button variant="danger" className='m-4' onClick={handleDesc} disabled={properties.disableDesc}>Decrease</Button>
+            <Button variant="success" onClick={handleAdd} disabled={properties.disableAdd}>Increase</Button>
         </div>
     )
 }
